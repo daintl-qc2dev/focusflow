95302e06983c406f9564097ea6af5752d1c3fc0c
Dai <dai.ntl@dgwasisa.com>
2026-06-10 13:44:29 +0700

    research: learn about variables, data types, typeof, functions

---
+++ js-basic.js
@@ -0,0 +1,17 @@
+// Kiểu String
+const firstName = "Nguyen";
+const lastName = "Dai";
+let job = "QC Engineer";
+
+// Kiểu Number
+let age = 26;
+const pi = 3.14;
+let score = 10;
+
+// Kiểu Boolean
+let isLearningJS = true;
+let hasPet = false;
+
+// Kiểu Undefined và Null
+let futureProject; // undefined
+let currentCar = null; // Hiện tại chưa có xe
