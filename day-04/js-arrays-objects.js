7e969e67691e4a2c05fc1006f5c9eca8311bfd3b
Dai <dai.ntl@dgwasisa.com>
2026-06-11 15:55:36 +0700

    feat: write function update data for array

--- day-04/js-arrays-objects.js
+++ day-04/js-arrays-objects.js
@@ -16,3 +16,21 @@ function createUser(newUser) {
 console.log("--- 1. Sao chep va them user moi ---");
 createUser({ id: 4, name: "Bui Tram Anh", email: "c@dgw.com", age: 37 });
 console.log(users);
+
+// ==========================================
+// 2. Hàm updateUser(id, updateData): Cập nhật thông tin user theo ID (Dùng map)
+// ==========================================
+function updateUser(id, updateData) {
+    users = users.map(user => {
+        if (user.id === id) {
+            // Nếu tìm thấy ID trùng khớp, gộp dữ liệu cũ với dữ liệu mới cập nhật
+            return { ...user, ...updateData };
+        }
+        return user; // Các user khác giữ nguyên
+    });
+    return users;
+}
+
+console.log("--- 2. Update data user ---");
+updateUser(2,{name: "Nguyen Van Hop", age: 16});
+console.log(users);
