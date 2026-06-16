1d6067275a7f0348224584cbe10e0db29d368943
Dai <dai.ntl@dgwasisa.com>
2026-06-16 16:11:14 +0700

    feat: write funtion check score

--- day-06/js-foundations.js
+++ day-06/js-foundations.js
@@ -10,12 +10,29 @@
 // countdown(10);
 
 //sumToN
-function sumToN(n){
-    let sum = 0;
-    for (let i = n; i > 0; i--){
-        sum = sum + i;
-    }
-    console.log(" sumToN(4)",sum);
-}
+// function sumToN(n){
+//     let sum = 0;
+//     for (let i = n; i > 0; i--){
+//         sum = sum + i;
+//     }
+//     console.log(" sumToN(4) la: ",sum);
+// }
 
-sumToN(4);
+// sumToN(4);
+
+//checkGrade
+function checkGrade(score){
+    if (score >= 9)
+        console.log("Xuat sac");
+    else if (score >= 8)
+        console.log("Gioi");
+    else if (score >= 6.5)
+        console.log("Kha");
+    else if (score >= 5)
+        console.log("Trung binh");
+    else
+        console.log("Yeu");
+}
+checkGrade(9);
+
+
