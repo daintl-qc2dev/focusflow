768e9421af2deaadba43ed8af07a60d5b4b80238
Dai <dai.ntl@dgwasisa.com>
2026-06-16 15:50:33 +0700

    feat: write function sumToN

--- day-06/js-foundations.js
+++ day-06/js-foundations.js
@@ -1,8 +1,21 @@
-function countdown(n){
+
+//countdown
+// function countdown(n){
+//     for (let i = n; i > 0; i--){
+//         console.log(i);
+//     }
+// }
+
+// console.log("Countdown tu 10");
+// countdown(10);
+
+//sumToN
+function sumToN(n){
+    let sum = 0;
     for (let i = n; i > 0; i--){
-        console.log(i);
+        sum = sum + i;
     }
+    console.log(" sumToN(4)",sum);
 }
 
-console.log("Countdown tu 10");
-countdown(10);
+sumToN(4);
