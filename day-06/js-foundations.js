5670e04cd1b3fb90588935a436635569e10ef9f8
Dai <dai.ntl@dgwasisa.com>
2026-06-16 16:14:54 +0700

    feat: write function return day of week

--- day-06/js-foundations.js
+++ day-06/js-foundations.js
@@ -1,24 +1,24 @@
 
 //countdown
-// function countdown(n){
-//     for (let i = n; i > 0; i--){
-//         console.log(i);
-//     }
-// }
+function countdown(n){
+    for (let i = n; i > 0; i--){
+        console.log(i);
+    }
+}
 
-// console.log("Countdown tu 10");
-// countdown(10);
+console.log("Countdown tu 10");
+countdown(10);
 
 //sumToN
-// function sumToN(n){
-//     let sum = 0;
-//     for (let i = n; i > 0; i--){
-//         sum = sum + i;
-//     }
-//     console.log(" sumToN(4) la: ",sum);
-// }
+function sumToN(n){
+    let sum = 0;
+    for (let i = n; i > 0; i--){
+        sum = sum + i;
+    }
+    console.log(" sumToN(4) la: ",sum);
+}
 
-// sumToN(4);
+sumToN(4);
 
 //checkGrade
 function checkGrade(score){
@@ -35,4 +35,35 @@ function checkGrade(score){
 }
 checkGrade(9);
 
+//dayOfWeek
+function dayOfWeek(n) {
+    switch (n) {
+        case 1:
+            console.log("Chủ Nhật");
+            break;
+        case 2:
+            console.log("Thứ Hai");
+            break;
+        case 3:
+            console.log("Thứ Ba");
+            break;
+        case 4:
+            console.log("Thứ Tư");
+            break;
+        case 5:
+            console.log("Thứ Năm");
+            break;
+        case 6:
+            console.log("Thứ Sáu");
+            break;
+        case 7:
+            console.log("Thứ Bảy");
+            break;
+        default:
+            console.log("Số không hợp lệ! Vui lòng nhập từ 1 đến 7.");
+    }
+}
 
+console.log("Test dayOfWeek");
+dayOfWeek(2);
+dayOfWeek(8);
