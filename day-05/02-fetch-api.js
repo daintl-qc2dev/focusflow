c8cd95f706d39c62ffb4d36e21f166a0abdf1576
Dai <dai.ntl@dgwasisa.com>
2026-06-17 10:37:18 +0700

    fix: fix commit for branch JSONPlaceholder callback → Promise → async/await

--- day-05/02-fetch-api.js
+++ day-05/02-fetch-api.js
@@ -1,17 +1,17 @@
 // Get users
-// async function getUsers(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+async function getUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-//         const users = await response.json();
+        const users = await response.json();
 
-//         console.log("Danh sach users", users);
-//     } catch (error) {
-//         console.log("Co loi gi do roi", error.message);
-//     }
-// }
+        console.log("Danh sach users", users);
+    } catch (error) {
+        console.log("Co loi gi do roi", error.message);
+    }
+}
 
-// getUsers();
+getUsers();
 
 //Get user by id
 async function getUserById(id){
@@ -20,13 +20,13 @@ async function getUserById(id){
 
         const user = await response.json();
 
-        return user;
-
         console.log(`User co id ${id} la: `, user.name);
+
+        return user;
     } catch (error) {
         console.log("Co loi gi do roi", error.message);
     }
-    
+   
 }
 
 getUserById(3);
@@ -34,8 +34,6 @@ getUserById(3);
 // Display detail user
 async function displayUserSummary(id){
     try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-
         const user = await getUserById(id);
 
         console.log(`Thong tin chi tiet nhan vien id ${id}`);
